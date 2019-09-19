78cb38de5be0e5d8b4126afb5100001a02a24c64
vnxtreme <nghoang2013@gmail.com>
2019-09-19 11:51:50 +0700

    update note

--- 7saturday-api.py
+++ 7saturday-api.py
@@ -6,6 +6,7 @@ start_index = 0
 end_index = 400
 gap = 20
 
+# gather list data
 data = []
 while start_index < end_index:
     response = requests.get(f"https://discovery.7saturday.com/api/v1/top_influencers?filters=%7B%22start_index%22:{start_index},%22end_index%22:{start_index+gap},%22random_index%22:31%7D").json()
@@ -13,9 +14,9 @@ while start_index < end_index:
     data += response['popular_influencers']
     start_index += gap
 
+#start writing
 folder = os.path.join(os.getcwd(), "Core/Input")
 os.chdir(folder)
-
 f = open('new-input.txt', 'w', encoding='utf-8', newline='\r\n')
 
 for influencer in data:
