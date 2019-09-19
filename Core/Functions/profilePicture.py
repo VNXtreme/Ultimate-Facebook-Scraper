e2c1e6c5664032715a42de2db995672677312e63
vnxtreme <nghoang2013@gmail.com>
2019-09-19 15:56:20 +0700

    bug fix: profile picture

--- Core/Functions/profilePicture.py
+++ Core/Functions/profilePicture.py
@@ -1,7 +1,13 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 def scrape_profile_picture(driver, isTimelineLayout: bool):
     if isTimelineLayout:
         pictureURL = driver.find_element_by_xpath(".//div[@class='photoContainer']/div/a/img").get_attribute('src')
     else:
-        pictureURL = driver.find_element_by_xpath(".//div[@class='_6tay']/img").get_attribute('src')
+        try:
+            pictureURL = driver.find_element_by_xpath(".//div[@class='_6tay']/img").get_attribute('src')
+        except NoSuchElementException:
+            pictureURL = driver.find_element_by_xpath(".//img[@class='_4jhq img']").get_attribute('src')
     
     return pictureURL
