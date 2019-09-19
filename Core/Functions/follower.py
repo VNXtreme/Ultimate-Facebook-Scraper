c12a78cef0eb4030a6c2b1bdc884eaae1706644c
vnxtreme <nghoang2013@gmail.com>
2019-09-19 17:17:47 +0700

    bug fix: catch no follower profile

--- Core/Functions/follower.py
+++ Core/Functions/follower.py
@@ -3,11 +3,14 @@ from Functions.common import extract_fb_username, extract_follower_number
 
 
 def scrape_follower(driver, isTimelineLayout: bool) -> int:
-    if(isTimelineLayout):
-        followerNumber = driver.find_element_by_xpath("//div[@class='_6a _6b plm']/span").text
-    else:
-        followerNumber = driver.find_element_by_xpath("//*[@id='pages_side_column']//div[@class='_4-u2 _6590 _3xaf _4-u8']/div[4]").text
-    followerNumber = followerNumber.split(' ', 1)[0].replace(',', '').replace('.', '')
+    try:
+        if(isTimelineLayout):
+            followerNumber = driver.find_element_by_xpath("//div[@class='_6a _6b plm']/span").text
+        else:
+            followerNumber = driver.find_element_by_xpath("//*[@id='pages_side_column']//div[@class='_4-u2 _6590 _3xaf _4-u8']/div[4]").text
+        followerNumber = followerNumber.split(' ', 1)[0].replace(',', '').replace('.', '')
+    except:
+        followerNumber = 0
     print(f"Followers: {followerNumber}")
 
     return followerNumber
