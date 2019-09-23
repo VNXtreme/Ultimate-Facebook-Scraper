f4f4751925473b56570f18264e9911c133eff61c
vnxtreme <nghoang2013@gmail.com>
2019-09-23 17:15:11 +0700

    working on string conversion

--- Core/Functions/common.py
+++ Core/Functions/common.py
@@ -6,6 +6,13 @@ def extract_follower_number(text):
     followerNumber = text.split(' ', 1)[0].replace(',', '')
     return followerNumber
 
+def convert_string_to_number(text):
+    numberInString = text.split(' ')[0]
+    if numberInString.find('K'):
+        numberInString.replace('.', '').replace(',', '') 
+        numberInString = numberInString * 1000
+    return numberInString
+
 def is_timeline_layout(driver):
     try:
         driver.find_element_by_class_name('timelineLayout')
