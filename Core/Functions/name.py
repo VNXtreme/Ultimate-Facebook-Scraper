ea7f38d73bfe06090038b2bc11710ec5d8602828
vnxtreme <nghoang2013@gmail.com>
2019-09-23 14:11:13 +0700

    format

--- Core/Functions/name.py
+++ Core/Functions/name.py
@@ -1,19 +1,23 @@
 def scrape_name(driver, isTimelineLayout: bool) -> str:
-    if(isTimelineLayout):
+    if isTimelineLayout:
         fullnameElement = driver.find_element_by_id('fb-timeline-cover-name')
     else:
-        fullnameElement = driver.find_element_by_xpath("//*[@id='seo_h1_tag']/a/span")
+        fullnameElement = driver.find_element_by_xpath(
+            "//*[@id='seo_h1_tag']/a/span")
     name = fullnameElement.text
     print(f"Name: {name}")
-        
+
     return name
 
+
 def scrape_username(driver, isTimelineLayout: bool) -> str:
-    if(isTimelineLayout):
-        fullUrl = driver.find_element_by_xpath('//span[@id="fb-timeline-cover-name"]/a').get_attribute('href')
+    if isTimelineLayout:
+        fullUrl = driver.find_element_by_xpath(
+            '//span[@id="fb-timeline-cover-name"]/a').get_attribute('href')
     else:
-        fullUrl = driver.find_element_by_xpath("//*[@id='seo_h1_tag']/a").get_attribute('href')
+        fullUrl = driver.find_element_by_xpath(
+            "//*[@id='seo_h1_tag']/a").get_attribute('href')
     username = fullUrl.split('facebook.com')[1].replace('/', '')
     print(f"Username: {username}")
-        
+
     return username
